929430de47afda8bcd13ff790a0c1e6adcbbe288
satake0916 <stk_halfmoon@icloud.com>
2023-11-19 16:19:03 +0900

    change url for mac

--- Formula/thue.rb
+++ Formula/thue.rb
@@ -1,8 +1,8 @@
 class Thue < Formula
   desc "cli translating day of week in English and Japanese"
   homepage "https://github.com/satake0916/thue"
-  url "https://github.com/satake0916/thue/files/13402447/thue-mac.tar.gz"
-  sha256 "e28cd716f5d1abf89444c2ffcf4568b4886fc844cdb9e5178e5babacca2068c9"
+  url "https://github.com/satake0916/thue/releases/download/v0.1.1_mac/thue-0.1.1-mac.tar.gz"
+  sha256 "efb6a6bdef2496b38db8f2b2157727ea2772e6a1eaa67d875509a40a58864891"
   version "0.1.1"
 
   def install
